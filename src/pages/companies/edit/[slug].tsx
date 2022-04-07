feba2cd7bfafd16379f647e33d6031dd2d41f1da
Thiago Spart <user000.000resu@gmail.com>
2022-04-07 11:56:26 -0300

    Add edit company page

---
+++ src/pages/companies/edit/[slug].tsx
@@ -0,0 +1,7 @@
+import { EditCompany } from "web/pages/Companies/EditCompany";
+
+import { LayoutsEnum } from "types/enums/layouts";
+
+EditCompany.layout = LayoutsEnum.DEFAULT;
+
+export default EditCompany;
